740af72bcf7b6bcfdd02b7b1220d83e424affbc3
Kwame Brown <okhabena@gamil.com>
2021-08-21 05:45:14 -0400

    my PC commit

---
+++ kys.java
@@ -0,0 +1,4 @@
+echo "Enter filename"
+read filename
+date
+echo "Thank you for adding new file '$filename'"
---
+++ kyser.java
@@ -0,0 +1,3 @@
+Enter filename
+Sat, Aug 21, 2021  5:38:58 AM
+Thank you for adding new [A[B[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D
